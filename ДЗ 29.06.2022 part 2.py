a90a153959fa4c546993431185d6fa212c613337
grigoriy31 <breslaw.spb@gmail.com>
2022-06-29 15:01:44 +0300

    ДЗ part 2. Дополнил предупреждением при введении не верного номера месяца.

---
+++ ДЗ 29.06.2022 part 2.py
@@ -0,0 +1,17 @@
+a = int(input("Введите номер месяца: "))
+if a < 1:
+    print("Не верный ввод!!!")
+elif a > 12:
+    print("Не верный ввод!!!")
+elif 1< a <= 7:
+    if a == 2:
+        print("ФЕВРАЛЬ")
+    elif(a % 2) == 0:
+        print("30")
+    else:
+        print("31")
+elif 7 < a <= 12:
+    if(a % 2) ==0:
+        print("31")
+    else:
+        print("30")
