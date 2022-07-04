bfabc4538bd21797e2e03ba658a2809d417eb018
grigoriy31 <breslaw.spb@gmail.com>
2022-07-04 22:50:19 +0300

    Код под коментом Б

---
+++ День #2/Дополнительные задачки.py
@@ -0,0 +1,34 @@
+#Задача 1 Строки
+#a
+def check_space(test):
+
+     i = -1
+     while i < len(test):
+          i += 1
+          if i != len(test):
+              a = test[i]
+               for y range
+          else:
+              continue
+     print(d)
+
+test = list(iter(input, ''))
+print(check_space(test))
+
+#b
+# def check_text(test):
+#     b = test[0]
+#     i = 0
+#     while i < len(test):
+#          i += 1
+#          if i < len(test):
+#               b = b + " " + test[i]
+#          elif i == len(test):
+#                b = b + "."
+#     print(b)
+#
+# test = list(iter(input, ''))
+# print(check_text(test))
+
+
+
