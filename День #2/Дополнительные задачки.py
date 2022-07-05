0123ff0decbed824f684d9eaf8b7e71d31214028
grigoriy31 <breslaw.spb@gmail.com>
2022-07-05 17:42:51 +0300

    ДЗ оба примера в одном файле. Если не затруднит посмотрите верно ли выполнены 2 последних кода в файле "рабочий файл"(те до которых не дошли во время занятия) Дополнил дополнительные задачи строки

--- День #2/Дополнительные задачки.py
+++ День #2/Дополнительные задачки.py
@@ -1,21 +1,22 @@
-#Задача 1 Строки
-#a
-def check_space(test):
+# #Задача 1 Строки
+# #a
+# n = int(input("Введите кол-во строк: "))
+# while n <= 0:
+#     n = int(input("Введите кол-во строк: "))
+# text = list()
+# k = 0
+# while k != n:
+#     a = input("введите текст: ")
+#     text.append(a.count(' '))
+#     k += 1
+#
+# k = 1
+# for chek_space in text:
+#     print("В строке " + str(k) + " ,количество пробелов: " + str(chek_space))
+#     k += 1
 
-     i = -1
-     while i < len(test):
-          i += 1
-          if i != len(test):
-              a = test[i]
-               for y range
-          else:
-              continue
-     print(d)
 
-test = list(iter(input, ''))
-print(check_space(test))
-
-#b
+# #b
 # def check_text(test):
 #     b = test[0]
 #     i = 0
@@ -28,7 +29,38 @@ print(check_space(test))
 #     print(b)
 #
 # test = list(iter(input, ''))
-# print(check_text(test))
+# check_text(test)
 
-
-
+#b part 2 (c функцией)
+def check_text(test):
+    i = 1
+    b = test[0]
+    while i < len(test):
+        i += 1
+        if i < len(test):
+            b = b + ' ' + test[i-1]
+        if i == len(test):
+            b = b + ' ' + test[i-1] + "."
+        else:
+            continue
+    print(b)
+n = int(input("Введите кол-во строк: "))
+while n <= 0:
+    n = int(input("Введите кол-во строк: "))
+test = list()
+k = 0
+while k != n:
+    a = input("введите текст: ")
+    test.append(a)
+    k += 1
+check_text(test)
+#b(без функции)
+# b = test[0]
+# i = 0
+# while i < len(test):
+#     i += 1
+#     if i < len(test):
+#         b = b + ' ' + str(test[i]) + "."
+#     else:
+#         continue
+#     print(b)
