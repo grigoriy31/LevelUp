8c2aa7917964fa5b485e40e7e3b536bfe4bc749a
grigoriy31 <breslaw.spb@gmail.com>
2022-07-22 21:48:06 +0300

    1

--- day8/list.py
+++ day8/list.py
@@ -1,11 +1,31 @@
-a = int(input('Введите число: '))
-b = []
-i = 2
-while i < a:
-    if a % i == 0:
-        b.append(i)
-        i += 1
+# a = int(input('Введите число: '))
+
+def list():
+    b = []
+    i = 2
+    while i < a:
+        if a % i == 0:
+            b.append(i)
+            i += 1
+        else:
+            i += 1
+    return b
+# print(b)
+def summ():
+    i = 0
+    b = list()
+    summ = 0
+    for i in b:
+        summ += i
+    if summ == a:
+        out = True
     else:
-        i += 1
-print(b)
+        out = False
+    return out
+for a in range(10000):
+    if summ(a) == True:
+        print(a)
+
+
+
 
