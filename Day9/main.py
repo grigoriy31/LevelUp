5f372595ec4e0687f47fab9dce8e5c785b604e6f
grigoriy31 <breslaw.spb@gmail.com>
2022-07-26 19:50:55 +0300

    2

--- Day9/main.py
+++ Day9/main.py
@@ -1,18 +1,27 @@
-a = [2, 4, 6, 3, 1]
-b = int(min(a))
-c = int(max(a))
-i = 0
-while i < len(a):
-    if a[i] == min(a):
-        a_min = i
-        i += 1
-    elif a[i] == max(a):
-        a_max = i
-        i += 1
-    else:
-        i += 1
-a[a_min] = c
-a[a_max] = b
+# a = [2, 4, 6, 3, 1]
+# b = int(min(a))
+# c = int(max(a))
+# i = 0
+# while i < len(a):
+#     if a[i] == min(a):
+#         a_min = i
+#         i += 1
+#     elif a[i] == max(a):
+#         a_max = i
+#         i += 1
+#     else:
+#         i += 1
+# a[a_min] = c
+# a[a_max] = b
+#
+# print(a)
+larger = [1, 2, 3, 5, 6]
+smaller = [3, 7]
 
-print(a)
+def isSublist():
+   for i in range(0, len(larger)):
+       if larger[i:i + len(smaller)] == smaller:
+           return True
+   return False
+print(isSublist())
 
