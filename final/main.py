1dc32fb4ef13f9ceba975b77ce6ad1a912332d99
grigoriy31 <breslaw.spb@gmail.com>
2022-07-31 01:12:40 +0300

    test

---
+++ final/main.py
@@ -0,0 +1,39 @@
+import openpyxl
+plans = {}
+def statistics():
+    stat = openpyxl.workbook('Statistic_Spb_2022.xlsx')
+
+
+
+
+def spb_part_1():
+    data = openpyxl.workbook('spb_part_1.xlsx')
+    sheet = data.active
+    row = 2
+    name_m = {}
+    while True:
+        name = sheet.cell(row=row, column = 1)
+        if type(name) == str:
+            name_m[name] = ['']
+            row += 1
+            return True
+        elif name == '':
+            return False
+
+
+def spb_part_2():
+    data = openpyxl.workbook('spb_part_1.xlsx')
+    sheet = data.active
+    row = 2
+    name_m = {}
+    while True:
+        name = sheet.cell(row=row, column=1)
+        if type(name) == str:
+            name_m[name] = ['']
+            row += 1
+            return True
+        elif name == '':
+            return False
+
+
+def sim_card():
